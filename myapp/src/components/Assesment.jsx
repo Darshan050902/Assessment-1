83354aa4be131fc4ab78070e9459df14f333b612
Darshan050902 <darshan.dixit@hyperface.co>
2024-01-16 04:18:55 -0500

    horizontal-bar-added

--- myapp/src/components/Assesment.jsx
+++ myapp/src/components/Assesment.jsx
@@ -57,6 +57,7 @@ export const Assesment = () => {
 
     
   return (
+    <>
     <div className='user-body'>
         <div className="user-display">
             <div className="header">
@@ -74,10 +75,11 @@ export const Assesment = () => {
                         <tr
                             key={ind}
                             onClick={(e) => {
-                                e.stopPropagation()
+                                e.stopPropagation();
                                 if (!usersInCard.includes(user)) {
-                                    setUsersInCard((prevUsers) => [...prevUsers, user]);
-                                    index.current = usersInCard.length;
+                                    const updatedUsersInCard = usersInCard.slice(0, index.current + 1);
+                                    setUsersInCard((prevUsers) => [...updatedUsersInCard, user]);
+                                    index.current = updatedUsersInCard.length;
                                     setNextFlag(true);
                                     if (index.current >= 1) setBackFlag(false);
                                 } else alert("User already present");
@@ -93,23 +95,46 @@ export const Assesment = () => {
         </div>
         {
             usersInCard.length>0 ? (
-                <div className='user-card-section'>
-                    <div className="card-header">
-                        <h4>User Card</h4>
+                <>
+                    <div className='user-card-section'>
+                        <div className="card-header">
+                            <h4>User Card</h4>
+                        </div>
+                        <div className="user-card">
+                            <h4>ID: {usersInCard[index.current]?.id}</h4>
+                            <h4>Name: {usersInCard[index.current]?.name}</h4>
+                            <h4>Username: {usersInCard[index.current]?.username}</h4>
+                            <h4>Email: {usersInCard[index.current]?.email}</h4>
+                        </div>
+                        <div className="control-buttons">
+                            {!backFlag ? <button onClick={handleGoBack}>Prev</button> : <button style={{backgroundColor:'grey', color:"white"}} onClick={handlEndClick}>Prev</button>}
+                            {!nextFlag ? <button onClick={handleGoNext}>Next</button> : <button style={{backgroundColor:'grey', color:"white"}} onClick={handlEndClick}>Next</button>}
+                        </div>
                     </div>
-                    <div className="user-card">
-                        <h4>ID: {usersInCard[index.current]?.id}</h4>
-                        <h4>Name: {usersInCard[index.current]?.name}</h4>
-                        <h4>Username: {usersInCard[index.current]?.username}</h4>
-                        <h4>Email: {usersInCard[index.current]?.email}</h4>
-                    </div>
-                    <div className="control-buttons">
-                        {!backFlag ? <button onClick={handleGoBack}>Prev</button> : <button style={{backgroundColor:'grey', color:"white"}} onClick={handlEndClick}>Prev</button>}
-                        {!nextFlag ? <button onClick={handleGoNext}>Next</button> : <button style={{backgroundColor:'grey', color:"white"}} onClick={handlEndClick}>Next</button>}
-                    </div>
-                </div>
+                </>
             ):null
         }
     </div>
+            {
+                usersInCard.length>0?(
+                    <div className="user-card-pagination">
+                        <div className="horizontal-bar">
+                            {
+                                usersInCard.map((user, ind)=>{
+                                    if(ind === index.current){
+                                        return <div key={ind} style={{backgroundColor:"black", color:"white"}} className="page">
+                                            {user.id}
+                                        </div>
+                                    }
+                                    return <div key={ind} className="page">
+                                        {user.id}
+                                    </div>
+                                })
+                            }
+                        </div>
+                    </div>
+                ):null
+            }
+    </>
   )
 }
